24f510f82d44b8ba8b32d56646ad594c65048c29
DmitryS1981 <dir2@odis.su>
2025-11-25 23:05:23 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args) {
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+        Employee[] employees = new Employee[5];
+
+        employees[0] = new Employee("Иванов Иван Иванович", "Рабочий",
+                "ivanov@company.com", "+7-911-123-45-67", 75000, 30);
+        employees[1] = new Employee("Петров Петр Петрович", "Программист",
+                "petrov@company.com", "+7-911-234-56-78", 85000, 25);
+        employees[2] = new Employee("Сидоров Иван Иванович", "Охранник",
+                "sidorov@company.com", "+7-911-345-67-89", 80000, 30);
+        employees[3] = new Employee("Иванова Мария Владимировна", "Директор",
+                "ivanova@company.com", "+7-911-456-78-90", 120000, 50);
+        employees[4] = new Employee("Сидорова Ольга Владимировна", "Бухгалтер",
+                "sidorova@company.com", "+7-911-567-89-01", 65000, 45);
+
+        System.out.println("СОТРУДНИКИ СТАРШЕ 40 ЛЕТ:\n");
+        for (Employee employee : employees) {
+            if (employee.age > 40) {
+                employee.printInfo();
+            }
+        }
+    }
 }
