6574534086cf34e8d518e4c997f97f7a70521281
DmitryS1981 <dir2@odis.su>
2025-11-25 23:04:18 +0300

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
-
 public class Employee {
+    String fullName;
+    String position;
+    String email;
+    String phone;
+    double salary;
+    int age;
+
+    public Employee(String fullName, String position, String email, String phone, double salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void printInfo() {
+        System.out.println("==========================================");
+        System.out.println("ФИО: " + fullName);
+        System.out.println("Должность: " + position);
+        System.out.println("Email: " + email);
+        System.out.println("Телефон: " + phone);
+        System.out.println("Зарплата: " + salary + " руб.");
+        System.out.println("Возраст: " + age + " лет");
+        System.out.println("==========================================");
+    }
 }
